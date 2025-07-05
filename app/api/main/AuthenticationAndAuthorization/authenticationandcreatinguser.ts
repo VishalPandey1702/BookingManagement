d7ab01e1dde561de2c75d982368ba2332354468c
zykoonvishalpandey <vishal.pandey@zykoontech.net>
2025-07-05 19:07:11 +0530

    updated

---
+++ app/api/main/AuthenticationAndAuthorization/authenticationandcreatinguser.ts
@@ -0,0 +1,47 @@
+"use server";
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient()
+
+export default async function authenticateOrCreateUser(
+  email: string,
+  fullName: string,
+  imageUrl?: string
+) {
+  try {
+    if (!email || !fullName || !imageUrl) {
+      return {
+        status: 400,
+        message: "Missing required fields.",
+      };
+    }
+
+    // Check if the user already exists
+
+    let user = await prisma.user.findUnique({
+      where: { user_email: email },
+    });
+
+    // If user does not exist, create a new user
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          user_email: email,
+          user_full_name: fullName,
+          user_image_url: imageUrl || null,
+        },
+      });
+    }
+
+    return {
+      status: 200,
+      data: user,
+    };
+  } catch (error) {
+    console.error("Error in authenticateOrCreateUser:", error);
+    return {
+      status: 500,
+      message: "An error occurred while authenticating or creating the user.",
+    };
+  }
+}
--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import authenticateOrCreateUser from "./app/api/main/AuthenticationAndAuthorization/authenticationandcreatinguser"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
@@ -8,6 +9,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       console.log("User email:", user.email)
       console.log("User name:", user.name)
       console.log("User image:", user.image)
+      if (!user.email || !user.name || !user.image) {
+        console.error("Missing required user information")
+        return false
+      }
+      const response = await authenticateOrCreateUser(
+        user.email,
+        user.name,
+        user.image
+      )
+      if (response.status !== 200) {
+        console.error("Error creating or authenticating user:", response.message)
+        return false
+      }
+      console.log("User authenticated or created successfully:", response.data)
+      // Optionally, you can store the user data in the session 
       return true
     },
   },
