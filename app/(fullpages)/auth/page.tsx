7d33a32a13d70c738245be30ce027e4335749f7e
gitsumansaurabh <itzsaurabhsuman@gmail.com>
2025-07-07 23:42:45 +0530

    merging

--- app/(fullpages)/auth/page.tsx
+++ app/(fullpages)/auth/page.tsx
@@ -1,12 +1,17 @@
 // app/(fullpages)/login/page.tsx
 "use client";
 
-import signInWithGoogle from "@/app/(fullpages)/auth/actions"
+import signInWithGoogle from "@/app/(fullpages)/auth/actions";
+import { useEffect } from "react";
 
 export default function SignIn() {
+  useEffect(() => {
+    signInWithGoogle();
+  }, []);
   return (
-    <form action={signInWithGoogle}>
-      <button type="submit">Continue with Google</button>
-    </form>
+    // <form action={signInWithGoogle}>
+    //   <button type="submit">Continue with Google</button>
+    // </form>
+    <></>
   );
 }
